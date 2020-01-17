b5ec12af8e8e5b7086c019ed03d1c6046fa21b11
george_chernov <georgiy.chernov@arcadia.spb.ru>
2020-01-17 19:18:39 +0300

    Some clean up
    Move App component files into components/app folder; Remove extra logo file;

--- todo-react-ts/src/components/app/App.tsx
+++ todo-react-ts/src/components/app/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./App.css";
-import { ListComponent } from "./components/list/List";
-import { Item } from "./models/Item";
+import { ListComponent } from "../list/List";
+import { Item } from "../../models/Item";
 
 interface State {
   items: Item[];
